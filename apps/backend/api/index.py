b1e7dc7040644fcdefc7323e0c906301884f6dfa
Konstantin Kornev <konstantin.kornev.1@gmail.com>
2023-09-13 18:24:19 -0500

    fix: remove authlib to manually verify code

--- apps/backend/api/index.py
+++ apps/backend/api/index.py
@@ -1,10 +1,10 @@
-from flask import Flask, url_for, Response, jsonify
-from authlib.integrations.flask_client import OAuth
+from flask import Flask, url_for, jsonify, request
 from os import environ
 from itsdangerous import URLSafeTimedSerializer
 from flask_cors import CORS
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs, quote
 from uuid import uuid4
+import requests
 
 
 app = Flask(__name__)
@@ -17,21 +17,13 @@ CORS(
     resources={r"/authorize": {"methods": ["GET", "POST", "OPTIONS"]}},
 )
 
-oauth = OAuth(app)
-
-github = oauth.register(
-    name="github",
-    client_id=environ["GITHUB_CLIENT_ID"],
-    client_secret=environ["GITHUB_CLIENT_SECRET"],
-    authorize_url="https://github.com/login/oauth/authorize",
-    access_token_url="https://github.com/login/oauth/access_token",
-    api_base_url="https://api.github.com/",
-    client_kwargs={
-        "scope": "public_repo",
-    },
-)
-
 serializer = URLSafeTimedSerializer(environ["SECRET_KEY"])
+
+callback_url = (
+    url_for("callback", _external=True)
+    if environ["ENV"] == "production"
+    else "http://192.168.1.18:8080/callback"  # also try changing to localhost instead both here and in github settings
+)
 
 
 @app.route("/")
@@ -41,27 +33,21 @@ def hello_world():
 
 @app.route("/authorize", methods=["GET", "POST", "OPTIONS"])
 def authorize():
-    github_state = str(uuid4())
-    is_prod = environ["ENV"] == "production"
-    callback_url = (
-        url_for("callback", _external=True)
-        if is_prod
-        else "http://192.168.1.18:8080/callback"
-    )
-    github_redirect_resp = github.authorize_redirect(callback_url, state=github_state)
-    redirect_url = github_redirect_resp.headers.get("Location")
+    client_id = environ["GITHUB_CLIENT_ID"]
+    scope = "public_repo"
+    state = str(uuid4())
 
-    parsed_redirect_url = urlparse(redirect_url)
-    redirect_url_params = parse_qs(parsed_redirect_url.query)
-    do_states_match = redirect_url_params.get("state", [None])[0] == github_state
+    github_auth_url = f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={quote(callback_url)}&scope={scope}&state={state}"
+    github_auth_url_params = parse_qs(urlparse(github_auth_url).query)
+    do_states_match = github_auth_url_params.get("state", [None])[0] == state
     if not do_states_match:
-        raise Exception("States do not match")
+        raise Exception("GitHub auth states do not match")
 
     session_id = serializer.dumps(str(uuid4()))
 
     return jsonify(
         {
-            "redirect_url": redirect_url,
+            "redirect_url": github_auth_url,
             "session_id": session_id,
         }
     )
@@ -69,9 +55,21 @@ def authorize():
 
 @app.route("/callback")
 def callback():
-    access_token = github.authorize_access_token().get(
-        "access_token"
-    )  # mismatching_state in dev environment
+    code = request.args.get("code", "")
+    client_id = environ["GITHUB_CLIENT_ID"]
+    client_secret = environ["GITHUB_CLIENT_SECRET"]
+    data = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "code": code,
+        "redirect_uri": callback_url,
+    }
+    headers = {"Accept": "application/json"}
+    response = requests.post(
+        "https://github.com/login/oauth/access_token", data=data, headers=headers
+    )
+    response_json = response.json()
+    access_token = response_json.get("access_token")
 
     return "Access token: " + access_token
 
