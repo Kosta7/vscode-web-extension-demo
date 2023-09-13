91683d84a13b4a7a870ec8a583fa3dc5031964f6
Konstantin Kornev <konstantin.kornev.1@gmail.com>
2023-09-13 13:40:43 -0500

    feat: implement /authorize and /callback endpoints

--- apps/backend/api/index.py
+++ apps/backend/api/index.py
@@ -1,10 +1,22 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, Response, request
 from authlib.integrations.flask_client import OAuth
 from os import environ
+from itsdangerous import URLSafeTimedSerializer
+from flask_cors import CORS
+from urllib.parse import urlparse, parse_qs
+from uuid import uuid4
 
 
 app = Flask(__name__)
-app.secret_key = environ["FLASK_SECRET_KEY"]
+app.secret_key = environ["SECRET_KEY"]
+
+if environ["ENV"] == "development":
+    print("CORS enabled")
+    CORS(
+        app,
+        origins="*",
+        resources={r"/authorize": {"methods": ["GET", "POST", "OPTIONS"]}},
+    )
 
 oauth = OAuth(app)
 
@@ -14,8 +26,13 @@ github = oauth.register(
     client_secret=environ["GITHUB_CLIENT_SECRET"],
     authorize_url="https://github.com/login/oauth/authorize",
     access_token_url="https://github.com/login/oauth/access_token",
-    api_base_url="https://api.github.com",
+    api_base_url="https://api.github.com/",
+    client_kwargs={
+        "scope": "public_repo",
+    },
 )
+
+serializer = URLSafeTimedSerializer(environ["SECRET_KEY"])
 
 
 @app.route("/")
@@ -23,17 +40,45 @@ def hello_world():
     return "Hello, World!"
 
 
-@app.route("/authorize")
+@app.route("/authorize", methods=["GET", "POST", "OPTIONS"])
 def authorize():
-    is_prod = environ["FLASK_ENV"] == "production"
-    _scheme = "https" if is_prod else "http"
-    redirect_uri = url_for("callback", _external=True, _scheme=_scheme)
-    return github.authorize_redirect(redirect_uri, scope="public_repo")
+    github_state = str(uuid4())
+    is_prod = environ["ENV"] == "production"
+    callback_url = (
+        url_for("callback", _external=True)
+        if is_prod
+        else "http://192.168.1.18:8080/callback"
+    )
+    github_redirect_resp = github.authorize_redirect(callback_url, state=github_state)
+    redirect_url = github_redirect_resp.headers.get("Location")
+
+    parsed_redirect_url = urlparse(redirect_url)
+    redirect_url_params = parse_qs(parsed_redirect_url.query)
+    do_states_match = redirect_url_params.get("state", [None])[0] == github_state
+    if not do_states_match:
+        raise Exception("States do not match")
+
+    session_id = serializer.dumps(str(uuid4()))
+
+    response = Response()
+    response.headers.add("redirect_url", redirect_url)
+    response.headers.add("session_id", session_id)
+
+    if environ["ENV"] == "development":
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add(
+            "Access-Control-Expose-Headers", "redirect_url, session_id"
+        )  # todo: check CORS only for development?
+
+    return response
 
 
 @app.route("/callback")
 def callback():
-    access_token = github.authorize_access_token().get("access_token")
+    access_token = github.authorize_access_token().get(
+        "access_token"
+    )  # mismatching_state in dev environment
+
     return "Access token: " + access_token
 
 
