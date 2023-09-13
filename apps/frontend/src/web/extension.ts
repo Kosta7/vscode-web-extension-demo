766656364357cffa13197672ba7719cb3ad6a950
Konstantin Kornev <konstantin.kornev.1@gmail.com>
2023-09-13 13:40:55 -0500

    feat: call /authorize from extension

--- apps/frontend/src/web/extension.ts
+++ apps/frontend/src/web/extension.ts
@@ -5,9 +5,32 @@ import { TreeDataProvider } from "./treeDataProvider";
 export function activate(context: vscode.ExtensionContext) {
   console.log("The extension is now active in the web extension host!");
 
+  const isDevelopment = vscode.env.machineId === "someValue.machineId";
+
   const authorizeAndFetchButton = vscode.commands.registerCommand(
     "demo.authorizeAndFetch",
-    () => {}
+    () => {
+      fetch(
+        `${
+          isDevelopment
+            ? "http://localhost:8080"
+            : "https://vscode-web-extension-demo-backend.vercel.app"
+        }/authorize`,
+        {
+          method: "POST",
+        }
+      ).then((response) => {
+        const redirectUrl = response.headers.get("redirect_url");
+        const sessionId = response.headers.get("session_id");
+
+        console.log("redirectUrl, sessionId", redirectUrl, sessionId, response);
+
+        if (redirectUrl && sessionId) {
+          vscode.env.openExternal(vscode.Uri.parse(redirectUrl));
+          context.secrets.store("sessionId", sessionId);
+        }
+      });
+    }
   );
 
   const treeView = vscode.window.createTreeView("demo.explorer", {
