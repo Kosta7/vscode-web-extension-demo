d9f746179a2759f3d78c9774fc4a2ac37a861b2b
Konstantin Kornev <konstantin.kornev.1@gmail.com>
2023-09-14 01:20:13 -0500

    feat: add aws secrets manager to store access_token and session_id; enable client authorization

--- apps/backend/api/index.py
+++ apps/backend/api/index.py
@@ -1,10 +1,11 @@
-from flask import Flask, url_for, jsonify, request
+from flask import Flask, url_for, jsonify, request, abort
 from os import environ
-from itsdangerous import URLSafeTimedSerializer
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
 from flask_cors import CORS
 from urllib.parse import urlparse, parse_qs, quote
 from uuid import uuid4
 import requests
+import boto3
 
 
 app = Flask(__name__)
@@ -14,7 +15,28 @@ app.secret_key = environ["SECRET_KEY"]
 CORS(
     app,
     origins="*",
-    resources={r"/authorize": {"methods": ["GET", "POST", "OPTIONS"]}},
+    resources={
+        r"/authorize": {"methods": ["GET", "POST", "OPTIONS"]},
+        r"/repos/<owner>/<repo>/files": {"methods": ["GET", "POST", "OPTIONS"]},
+    },
+)
+
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization,Content-Type"
+    return response
+
+
+aws_session = boto3.session.Session()
+aws_client = aws_session.client(
+    service_name="secretsmanager",
+    region_name=environ["MY_AWS_REGION_NAME"],
+    aws_access_key_id=environ["MY_AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key=environ["MY_AWS_SECRET_ACCESS_KEY"],
+    endpoint_url=environ["MY_AWS_ENDPOINT_URL"],
 )
 
 serializer = URLSafeTimedSerializer(environ["SECRET_KEY"])
@@ -27,6 +49,12 @@ def hello_world():
 
 @app.route("/authorize", methods=["GET", "POST", "OPTIONS"])
 def authorize():
+    try:
+        session_id = serializer.dumps(str(uuid4()))
+        aws_client.create_secret(Name=session_id, SecretString="null")
+    except Exception as e:
+        abort(500, f"AWS Secret creation failed - {str(e)}")
+
     client_id = environ["GITHUB_CLIENT_ID"]
     scope = "public_repo"
     callback_url = (
@@ -34,15 +62,8 @@ def authorize():
         if environ["ENV"] == "production"
         else "http://192.168.1.18:8080/callback"  # also try changing to localhost instead both here and in github settings
     )
-    state = str(uuid4())
 
-    github_auth_url = f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={quote(callback_url)}&scope={scope}&state={state}"
-    github_auth_url_params = parse_qs(urlparse(github_auth_url).query)
-    do_states_match = github_auth_url_params.get("state", [None])[0] == state
-    if not do_states_match:
-        raise Exception("GitHub auth states do not match")
-
-    session_id = serializer.dumps(str(uuid4()))
+    github_auth_url = f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={quote(callback_url)}&scope={scope}&state={session_id}"
 
     return jsonify(
         {
@@ -53,8 +74,26 @@ def authorize():
 
 
 @app.route("/callback")
-def callback():
+def callback():  # check the origin of the request?
+    state = request.args.get("state", "")
     code = request.args.get("code", "")
+
+    if not state or not code:
+        abort(400, "Missing state or code")
+
+    try:
+        serializer.loads(state, max_age=600)
+    except (SignatureExpired, BadSignature):
+        abort(401, "Invalid or expired state")
+
+    try:
+        session_secret = aws_client.get_secret_value(SecretId=state)
+    except Exception as e:
+        abort(401, f"Unauthorized: AWS Secret fetch failed - {str(e)}")
+
+    if "SecretString" not in session_secret:
+        abort(401, "Invalid state")
+
     client_id = environ["GITHUB_CLIENT_ID"]
     client_secret = environ["GITHUB_CLIENT_SECRET"]
     callback_url = (
@@ -69,13 +108,54 @@ def callback():
         "redirect_uri": callback_url,
     }
     headers = {"Accept": "application/json"}
-    response = requests.post(
-        "https://github.com/login/oauth/access_token", data=data, headers=headers
-    )
-    response_json = response.json()
-    access_token = response_json.get("access_token")
 
-    return "Access token: " + access_token
+    try:
+        response = requests.post(
+            "https://github.com/login/oauth/access_token", data=data, headers=headers
+        )
+    except Exception as e:
+        abort(401, f"GitHub access token fetch failed - {str(e)}")
+
+    access_token = response.json().get("access_token")
+
+    try:
+        aws_client.put_secret_value(SecretId=state, SecretString=access_token)
+    except Exception as e:
+        abort(500, f"AWS Secret update failed - {str(e)}")
+
+    return ("Yes", 200)
+
+
+@app.route("/repos/<owner>/<repo>/files")
+def get_repo(owner, repo):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        abort(401, "Authorization header not found")
+
+    session_id = auth_header.split(" ")[1]
+
+    try:
+        session_secret = aws_client.get_secret_value(SecretId=session_id)
+    except Exception as e:
+        abort(401, f"Unauthorized: AWS Secret fetch failed - {str(e)}")
+
+    if "SecretString" not in session_secret:
+        abort(401, "Unauthorized: SecretString not found")
+
+    access_token = session_secret["SecretString"]
+
+    if not access_token or access_token == "null":
+        abort(401, "Unauthorized: access_token not found")
+
+    return access_token
+
+    # headers = {"Authorization": f"token {access_token}"}
+    # response = requests.get(
+    #     f"https://api.github.com/repos/{owner}/{repo}/contents/",
+    #     headers=headers,
+    # )
+    # response_json = response.json()
+    # return jsonify(response_json)
 
 
 if __name__ == "__main__":
--- apps/frontend/src/web/extension.ts
+++ apps/frontend/src/web/extension.ts
@@ -7,20 +7,40 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.showInformationMessage("The extension is now active!");
 
   const isDevelopment = vscode.env.machineId === "someValue.machineId";
+  const apiOrigin = isDevelopment
+    ? "http://localhost:8080"
+    : "https://vscode-web-extension-demo-backend.vercel.app";
+
+  let pollingIntervalId: NodeJS.Timeout;
 
   const authorizeAndFetchButton = vscode.commands.registerCommand(
     "demo.authorizeAndFetch",
     () => {
-      fetch(
-        `${
-          isDevelopment
-            ? "http://localhost:8080"
-            : "https://vscode-web-extension-demo-backend.vercel.app"
-        }/authorize`,
-        {
-          method: "POST",
+      const poll = async () => {
+        try {
+          const sessionId = await context.secrets.get("sessionId");
+          if (!sessionId) return;
+
+          const response = await fetch(
+            `${apiOrigin}/repos/${"ownerId"}/${"repoId"}/files`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${sessionId}`,
+              },
+            }
+          );
+          if (response.status === 200) {
+            clearInterval(pollingIntervalId);
+          }
+        } catch (error) {
+          console.error(error);
         }
-      )
+      };
+
+      fetch(`${apiOrigin}/authorize`, {
+        method: "POST",
+      })
         .then((response) => response.json())
         .then(({ redirect_url: redirectUrl, session_id: sessionId }) => {
           vscode.window.showInformationMessage(
@@ -31,6 +51,8 @@ export function activate(context: vscode.ExtensionContext) {
           if (redirectUrl && sessionId) {
             vscode.env.openExternal(vscode.Uri.parse(redirectUrl));
             context.secrets.store("sessionId", sessionId);
+
+            pollingIntervalId = setInterval(poll, 1000);
           }
         })
         .catch((err) => {
